82d2cdc675aa08e0ad9e0e3b8aa584f895bb728f
mabel <mabelare293@gmail.com>
2025-01-23 17:56:32 +0100

    sections

--- src/sections/Adage.jsx
+++ src/sections/Adage.jsx
@@ -5,53 +5,53 @@ import video from "../assets/myVideos/video3.mp4";
 
 const Adage = () => {
   return (
-    <section className="mt-64">
-      <div className="flex justify-between px-6 py-12 items-start -mt-52 mb-12">
-        <p className="text-[40px] font-bold text-gray-900  leading-tight ml-8">
+    <section className="mt-16 md:mt-32 lg:mt-64">
+      <div className=" flex flex-col md:flex-row justify-between px-6 py-12 items-start md:-mt-52 mb-12">
+        <p className="text-[20px] md:text-[30px] lg:text-[40px] font-bold text-gray-900 leading-tight ml-8">
           BASIC/DEPT <sup>®</sup>is a global branding <br /> and digital design
           agency building <br />
           products, services, and eCommerce <br /> experiences that turn
           cultural values <br /> into company value.
           <br />
-          <span className="px-6 py-2 mt-12 border font-bold border-gray-600 text-[13px] rounded-3xl hover:bg-gray-400">
+          <span className="px-4 py-2 mt-4 md:mt-8 lg:mt-12 border font-bold border-gray-600 text-[10px] md:text-[12px] lg:text-[13px] rounded-3xl hover:bg-gray-400">
             SEE THE WORK
           </span>
         </p>
-        <h1 className="text-[210px] text-gray-900 font-extrabold -mt-16 mr-4">
+        <h1 className="text-[100px] md:text-[150px] lg:text-[210px] text-gray-900 font-extrabold -mt-8 md:-mt-16 mr-4">
           B/D®
         </h1>
       </div>
 
-      <div className="flex justify-between items-center w-full gap-4">
-        <div className="group w-1/3 h-4/5 ml-16">
+      <div className="flex md:flex-row justify-between items-center w-full gap-4">
+        <div className="group  md:w-1/3 h-4/5 ml-4 md:ml-16">
           <img
             src={pic1}
             alt="pic1"
             className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105"
           />
 
-          <p className="text-[25px] mt-6 font-bold group-hover:underline">
+          <p className="text-[20px] md:text-[25px] mt-4 md:mt-6 font-bold group-hover:underline">
             PATAGONIA
           </p>
           <br />
-          <p className="text-[15px] -mt-4 font-medium">
+          <p className="text-[12px] md:text-[15px] -mt-2 md:-mt-4 font-medium">
             AN ECOMMERC EXPERIENCE <br />
             DRIVEN BY PATAGONIA'S <br />
             BRAND MISSION
           </p>
         </div>
-        <div className="group w-1/3 h-4/5">
+        <div className=" group  md:w-1/3 h-4/5 ml-4 md:ml-16">
           <img
             src={pic2}
             alt="pic2"
             className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105"
           />
 
-          <p className="text-[25px] mt-6 font-bold group-hover:underline">
+          <p className=" text-[20px] md:text-[25px] mt-4 md:mt-6 font-bold group-hover:underline">
             WILSON
           </p>
           <br />
-          <p className="text-[15px] -mt-4 font-medium">
+          <p className="text-[12px] md:text-[15px] -mt-2 md:-mt-4 font-medium">
             A CENTURY-OLD SPORTS
             <br />
             BRAND FINDING ITS PLACE IN
@@ -59,7 +59,7 @@ const Adage = () => {
             CULTURE
           </p>
         </div>
-        <div className="group w-1/3 h-4/5 mt-6 mr-16">
+        <div className="group w-1/3 md:w-1/3 h-4/5 mt-6 mr-16">
           <a href="/" className="">
             <video
               autoPlay
@@ -71,11 +71,11 @@ const Adage = () => {
             </video>
           </a>
 
-          <p className="text-[25px] mt-6 font-bold group-hover:underline">
+          <p className="text-[20px] md:text-[25px] mt-4 md:mt-6 font-bold group-hover:underline">
             GOOGLE STORE
           </p>
           <br />
-          <p className="text-[15px] -mt-4 font-medium">
+          <p className="text-[12px] md:text-[15px] -mt-2 md:-mt-4 font-medium">
             AN ECOMMERCE EXPERIENCE
             <br />
             HELPING GOOGLE BRING ITS <br />
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,36 +4,36 @@ import webby from "../assets/images/webby1.jpg";
 import campaign from "../assets/images/campaignlogo.webp";
 const Hero = () => {
   return (
-    <section className="mt-64">
+    <section className="mt-16 md:mt-32 lg:mt-64">
       <div className="flex justify-between items-center  ">
-        <div className="ml-48">
+        <div className="ml-8 md:ml-24 lg:ml-48 mb-8 md:mb-0">
           <img src={adage} alt="adage logo" width={200} />
-          <p className="text-center text-[12px] -mb-4 mt-12 font-semibold">
+          <p className="text-center text-[10px] md:text-[12px] lg:text-[14px] -mb-2 md:-mb-4 mt-6 md:mt-12 font-semibold">
             DESIGN AND BRANDING
           </p>
           <br />
-          <p className="text-center text-[12px] font-semibold">
+          <p className="text-center text-[10px] md:text-[12px] lg:text-[14px] font-semibold">
             AGENCY OF THE YEAR
           </p>
         </div>
-        <div className="mr-10">
+        <div className="mr-8 md:mr-24 lg:mr-48 mb-8 md:mb-0">
           <img src={webby} alt="webby logo" width={200} />
-          <p className="text-center text-[12px] mt-8 font-semibold">
+          <p className="text-center text-[10px] md:text-[12px] lg:text-[14px] mt-6 md:mt-8 font-semibold">
             AGENCY OF THE YEAR
           </p>
         </div>
-        <div className="mr-48">
+        <div className="mr-8 md:mr-24 lg:mr-48">
           <img src={campaign} alt="campaign logo" width={200} />
-          <p className="text-center text-[12px] -mb-4 mt-12 font-semibold">
+          <p className="text-center text-[10px] md:text-[12px] lg:text-[14px] -mb-2 md:-mb-4 mt-6 md:mt-12 font-semibold">
             DIGITAL INNOVATION AGENCY
           </p>
           <br />
-          <p className="text-center text-[12px] font-semibold">
+          <p className="text-center text-[10px] md:text-[12px] lg:text-[14px] font-semibold">
             OF THE YEAR FINALIST
           </p>
         </div>
       </div>
-      <div className="w-9/10 border-b border-gray-600 mt-56 ml-16"></div>
+      <div className="w-9/10 border-b border-gray-600 mt-56 ml-16 sm:-mt-20  "></div>
     </section>
   );
 };
