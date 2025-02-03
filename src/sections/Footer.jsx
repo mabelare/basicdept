adff10e434af5f485212a7a1ae679e8ab79142e7
mabel <mabelare293@gmail.com>
2025-02-03 05:10:43 +0100

    margin left footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <div>
       <footer className=" gap-4  bg-zinc-950 pt-10 pb-4">
         <div className=" flex flex-col lg:flex-row justify-between font-bold text-[12px] lg:text-[10px] text-gray-600 hover:text-white">
-          <h3 className="lg:ml-12 ml-32 -mt-4 lg:-mt-0 md:ml-64">
+          <h3 className="lg:ml-12 ml-36 -mt-4 lg:-mt-0 md:ml-64">
             BASIC/DEPT<sup>®</sup>, Inc 10 - 25©
           </h3>
-          <h3 className="ml-20 md:ml-52">
+          <h3 className="ml-24 md:ml-52">
             Easy to understand, impossible to ignore.<sup>™</sup>
           </h3>
-          <h3 className="lg:mr-12 ml-36 md:ml-[270px]">
+          <h3 className="lg:mr-12 ml-40 md:ml-[270px]">
             Terms, Privacy Policy
           </h3>
         </div>
